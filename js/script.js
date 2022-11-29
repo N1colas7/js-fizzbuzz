cda2bfa062bcca37466163ecb03d6cb7131dcf03
N1colas7 <nicolasnd9897@gmail.com>
2022-11-29 18:32:03 +0100

    finish ex

--- js/script.js
+++ js/script.js
@@ -4,15 +4,15 @@ const container = document.querySelector('.container');
 for( let i = 1; i <= 100; i++){
     let element;
     if( i%3 == 0 & i%5 == 0 ) {
-         element = '<div class="box box red"></div>';
+         element = `<div class="box box-red"> ${i} </div>`;
     }else if ( i%3 == 0 ){
-         element = '<div class="box box orange"></div>';
+         element = `<div class="box box-orange"> ${i} </div>`;
     }else if( i%5 == 0 ){
-         element = '<div class="box box green"></div>';
+         element = `<div class="box box-green"> ${i} </div>`;
     }else{
-         element = '<div class="box box blue"></div>';
+         element = `<div class="box box-blue"> ${i} </div>`;
 
     }
-}
+        container.innerHTML += element ;
+    }
 
-    container.innerHTML += element ;
